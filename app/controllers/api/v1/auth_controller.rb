84a598aab95d6161de6484948c1dbc817dcb288a
Jesse Novotny <jessenovotny@gmail.com>
2017-03-07 17:03:12 -0700

    cleaned up stuff

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::AuthController < Api::V1::ApplicationController
 
   def is_signed_in?
     if user_signed_in?
-      render :json => {"signed_in" => true, "user" => current_user}.to_json()
+      render :json => current_user.to_json()
     else
-      render :json => {"signed_in" => false}.to_json()
+      render :json => false
     end
   end
 end
